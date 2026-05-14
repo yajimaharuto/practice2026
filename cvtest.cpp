899ff7103c8613f83724c8aa700a4085be8e92cd
Haruto Yajima <26m217@st.takushoku-u.ac.jp>
2026-05-14 10:57:36 +0900

    aaa

--- cvtest.cpp
+++ cvtest.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+auto clamp = [](int val) {
+  if (val > 255) {
+    val = 255;
+  }
+  if (val < 0) {
+    val = 0;
+  }
+  return val;
+};
+
+cv::Mat rgb2ycbcr(cv::Mat image) {
+  // RGB色空間からYCbCr色空間に変換 imgはsharllowコピーなのでok
+  const int width = image.cols;
+  const int height = image.rows;
+  const int nc = image.channels();
+  const int stride = image.step;
+  uint8_t* pixel = image.data;
+  for (int i = 0; i < height; ++i) {
+    for (int j = 0; j < width; ++j) {
+      int B = pixel[i * stride + j * nc + 0];
+      int G = pixel[i * stride + j * nc + 1];
+      int R = pixel[i * stride + j * nc + 2];
+
+      int Y, Cb, Cr;
+      Y = 0.299 * R + 0.587 * G + 0.114 * B;
+      Cb = -0.1687 * R + -0.3313 * G + 0.5 * B;
+      Cr = 0.5 * R + -0.4187 * G + -0.0813 * B;
+      Cb += 128;
+      Cr += 128;
+
+      pixel[i * stride + j * nc + 0] = Y;
+      pixel[i * stride + j * nc + 1] = Cr;
+      pixel[i * stride + j * nc + 2] = Cb;
+    }
+  }
+  return image;
+}
+
 int main() {
-  cv::Mat image = cv::imread("./barbara.ppm", cv::IMREAD_ANYCOLOR);
+  // cv::Mat image = cv::imread("./barbara.ppm", cv::IMREAD_ANYCOLOR);
+  cv::Mat image =
+      cv::imread("H:/practice2026/barbara.ppm", cv::IMREAD_ANYCOLOR);
   // cv::Mat image(480, 640, CV_8UC3);
   if (image.empty()) return EXIT_FAILURE;
 
@@ -14,31 +54,34 @@ int main() {
   std::cout << "height = " << image.rows << std::endl;
 
   auto pixel = image.data;
-  auto clamp = [](int val) {
-    if (val > 255) {
-      val = 255;
-    }
-    if (val < 0) {
-      val = 0;
-    }
-    return val;
-  };
+  image = rgb2ycbcr(image);
+  std::vector<cv::Mat> ycrcb;
+  cv::split(image, ycrcb);  //[0] = y, [1] = Cr, [2] = Cb
+  cv::resize(ycrcb[1], ycrcb[1], cv::Size(), 0.5, 0.5);
+  cv::resize(ycrcb[2], ycrcb[2], cv::Size(), 0.5, 0.5);
 
-  for (int i = 0; i < height; ++i) {
-    for (int j = 0; j < width; ++j) {
-      int B = pixel[i * stride + j * nc + 0];
-      int G = pixel[i * stride + j * nc + 1];
-      int R = pixel[i * stride + j * nc + 2];
-      pixel[i * stride + j * nc + 0] = R;
-      pixel[i * stride + j * nc + 2] = B;
-      B = clamp(B * 4);
-      G = clamp(G * 4);
-      R = clamp(R * 4);
+  for (int y = 0; y < height; y += 8) {
+    for (int x = 0; x < width; x += 8) {
+      // 起点の座標(x, y)で、8*8の領域を切り出す
+      cv::Mat tmp = ycrcb[0](cv::Rect(x, y, 8, 8));
+      cv::Mat blk;
+      tmp.convertTo(blk, CV_32F);
+      blk -= 128.0f;
+      // DCT
+
+      // 量子化
     }
   }
+
+  int x, y;
+  x = 447;
+  y = 68;
+  cv::imshow("loaded image", ycrcb[0](cv::Rect(x, y, 100, 100)));
+
+  // cv::cvtColor(image, image, cv::COLOR_BGR2YCrCb)
   cv::imshow("loaded image", image);
 
   cv::waitKey(0);
-  cv::destroyAllwindows();
+  cv::destroyAllWindows();
   return EXIT_SUCCESS;
 }
